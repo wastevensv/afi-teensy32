be62a01e5ed116d85ad7553bf2690089ddaef96c
William Stevens <contact@wastevensv.com>
2017-10-10 15:42:53 -0400

    Removed unnecessary files.

--- bsp/drivers/wdog/wdog.d
+++
@@ -1,6 +0,0 @@
-hw/teensy32/bsp/drivers/wdog/wdog.o: hw/teensy32/bsp/drivers/wdog/wdog.c \
- hw/teensy32/include/common.h hw/teensy32/include/arm_cm4.h \
- hw/teensy32/include/freedom.h hw/teensy32/include/mcg.h \
- hw/teensy32/include/MK20DZ10.h hw/teensy32/include/assert.h \
- hw/teensy32/include/io.h hw/teensy32/include/startup.h \
- hw/teensy32/bsp/drivers/wdog/wdog.h
